e76a5487d6bf7f0c1538b093628c21813e3c3ea3
Kon Vladislav <vstaff22b@gmail.com>
2024-12-24 13:02:43 +1100

    из нового методы поворотов влево вправо балансировка добавление удаление узла освобождение памяти (деструктор)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <fstream>
+//#include <string>
+#include <locale.h>
+#include <regex>
+
 using namespace std;
 
 // node key field 1 - degree
@@ -23,7 +28,7 @@ public:
 		}
 	}
 
-	Group(Degree p_degree, int unsigned p_number[4]) {
+	Group(Degree p_degree, int p_number[4]) {
 		degree = p_degree;
 		
 		for (int i = 0; i < 4; ++i) {
@@ -76,6 +81,30 @@ public:
 	}
 };
 
+std::ostream& operator << (std::ostream& os, const Node& node) {
+	char degree;
+
+	switch (node.key.degree) {
+	case B:
+		degree = 'B';
+		break;
+	case M:
+		degree = 'M';
+		break;
+	case S:
+		degree = 'S';
+		break;
+	}
+
+	string number;
+
+	for (int i = 0; i < 4; ++i) {
+		number += std::to_string(node.key.number[i]);
+	}
+
+	return os << degree << number;
+}
+
 class AVLTree {
 private:
 	Node* root;
@@ -89,14 +118,14 @@ private:
 		return node == nullptr ? 0 : getHeight(node->left) - getHeight(node->right);
 	}
 
-	void fixHeight(Node* node) {
+	void fixHeight(Node* &node) {
 		int leftChildHeight = getHeight(node->left);
 		int rightChildHeight = getHeight(node->right);
 		node->height = max(leftChildHeight, rightChildHeight) + 1;
 	}
 
 	// right rotation
-	Node* rotateRight(Node* p) {
+	Node* rotateRight(Node* &p) {
 		Node* q = p->left;
 		p->left = q->right;
 		q->right = p;
@@ -106,7 +135,7 @@ private:
 	}
 
 	// left rotation 
-	Node* rotateLeft(Node* q) {
+	Node* rotateLeft(Node* &q) {
 		Node* p = q->right;
 		q->right = p->left;
 		p->left = q;
@@ -115,28 +144,28 @@ private:
 		return p;
 	}
 
-	Node* balance(Node* p)  {
+	Node* balance(Node* &p)  {
 		fixHeight(p);
 
 		if (getBalance(p) == 2) {
 			if (getBalance(p->right) < 0) {
-				p->right = rotateRight(p->right);
+				p->right = rotateLeft(p->right);
 			}
-			return rotateLeft(p);
+			return rotateRight(p);
 		}
 
 		if (getBalance(p) == -2) {
 			if (getBalance(p->left) > 0) {
-				p->left = rotateLeft(p->left);
+				p->left = rotateRight(p->left);
 			}
-			return rotateRight(p);
+			return rotateLeft(p);
 		}
 
 		return p;
 	}
 
-	Node* insert(Node* p, Group k) {
-		if (!p) {
+	Node* insert(Node* &p, const Group k) {
+		if (p == nullptr) {
 			return new Node(k);
 		}
 
@@ -151,11 +180,11 @@ private:
 		return balance(p);
 	}
 
-	Node* getMin(Node* p) {
+	Node* getMin(Node* &p) {
 		return p->left != nullptr ? getMin(p->left) : p;
 	}
 
-	Node* removeMin(Node* p) {
+	Node* removeMin(Node* &p) {
 		if (p->left == nullptr) {
 			return p->right;
 		}
@@ -164,7 +193,7 @@ private:
 		return balance(p);
 	}
 
-	Node* remove(Node* p, Group k) // удаление ключа k из дерева p
+	Node* remove(Node* &p, Group k) // удаление ключа k из дерева p
 	{
 		if (p == nullptr) {
 			return nullptr;
@@ -278,12 +307,170 @@ private:
 
 	//}
 
+	// центрированный 
+	Node* LNR(Node* node) {
+		if (node == nullptr) {
+			return nullptr;
+		}
+
+		LNR(node->left);
+		cout << *node << endl;
+		LNR(node->right);
+	}
+
+	// прямой 
+	Node* NLR(Node* node) {
+		if (node == nullptr) {
+			return nullptr;
+		}
+
+		cout << *node;
+		NLR(node->left);
+		NLR(node->right);
+	}
+
+	// обратный 
+	Node* LRN(Node* node) {
+		if (node == nullptr) {
+			return nullptr;
+		}
+
+		LRN(node->left);
+		LRN(node->right);
+		cout << *node;
+	}
+
 public:
+	AVLTree() {
+		root = nullptr;
+	}
+
 	void insert(Group key) {
-		insert(root, key);
+		root = insert(root, key);
 	}
 
 	void remove(Group key) {
 		remove(root, key);
 	}
+
+	void LNR() {
+		LNR(root);
+	}
+
+	void NLR() {
+		NLR(root);
+	}
+
+	void LRN() {
+		LRN(root);
+	}
 };
+
+// method for validating input data in text file
+bool validateInput(string fileName) {
+	std::ifstream file(fileName);
+	string line;
+
+	if (file.is_open()) {
+		while (std::getline(file, line)) {
+			if (line.length() != 5) {
+				cout << "каждый ключ должен состоять из 5 символов";
+				file.close();
+				return false;
+			}
+
+			char degree = line.at(0);
+			string number = line.substr(1);
+
+			if (degree != 'B' && degree != 'M' && degree != 'S') {
+				cout << "первый символ ключа должен быть одной из перечисленных букв: B, M, S\n";
+				file.close();
+				return false;
+				
+			}
+
+			for (int i = 0; i < 4; ++i) {
+				if (!isdigit(number[i])) {
+					cout << "последующие после первого символы ключа должны быть цифрами\n";
+					file.close();
+					return false;
+				}
+			}
+
+			cout << degree << ':' << number[0] << number[1] << number[2] << number[3] << endl;
+		}
+	}
+	else {
+		cout << "нет файла с именем: " + fileName;
+		return false;
+	}
+
+	file.close();
+	return true;
+}
+
+bool validateKey(string key) {
+	const regex pattern("[BMS]\\d{4}");
+	
+	return regex_match(key, pattern);
+}
+
+std::vector<int> getDigits(string str_digits) {
+	vector<int> digits;
+
+	for (char character : str_digits) {
+		digits.push_back(character - '0');
+	}
+
+	return digits;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	AVLTree tree;
+
+	std::ifstream file("./input.txt");
+	string line;
+
+	if (!file.is_open()) {
+		cout << "нет файла с таким именем";
+		return 0;
+	}
+
+	while (std::getline(file, line)) {
+		if (!validateKey(line)) {
+			cout << "некорректный ключ";
+			return 0;
+		}
+
+		char degree = line.at(0);
+		Degree groupDegree;
+
+		switch (degree) {
+		case 'B':
+			groupDegree = B;
+			break;
+		
+		case 'M':
+			groupDegree = M;
+			break; 
+
+		case 'S':
+			groupDegree = S;
+			break;
+		}
+
+		string number = line.substr(1);
+		int groupNumber[4];
+
+		for (int i = 0; i < 4; ++i) {
+			groupNumber[i] = number[i] - '0';
+		}
+
+
+		Group key = Group(groupDegree, groupNumber);
+
+		tree.insert(key);
+	}
+}
